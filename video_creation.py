e634e852e170065f6a343b983587b2cc3547c70f
SabaJazi <saba.yousefian72@gmail.com>
2022-06-03 12:11:20 -0500

    creating original videos from frames + fixing the issues

--- video_creation.py
+++ video_creation.py
@@ -2,18 +2,31 @@ import numbers
 from tokenize import Number
 import cv2
 import os
+import numpy as np
 
+# frames directory for input
 image_folder = 'd:/LTT/frames'
-video_name = 'original.avi'
-#what is the type of os.listdr??
-images = [img for img in sorted(os.listdir(image_folder)) if img.endswith(".jpg")]
-frame = cv2.imread(os.path.join(image_folder, images[0]))
-height, width, layers = frame.shape
 
+# output directory to save the video
+video_name = 'original_7fps.avi'
+
+imgs=[]
+images = [img for img in os.listdir(image_folder)  if img.endswith(".jpg")]
+# removing the .jpg and adding the int of images names to list
+# if we read the images names the 10 will come after 1 instead of 2 after 1
+for img in images:
+    img=img[:-4]
+    imgs.append(int(img))
+
+imgs=sorted(imgs)
+np_img=np.array(imgs)
+# getting the shape of frames by reading one frame
+frame=cv2.imread(os.path.join(image_folder, str(np_img[0])+'.jpg'))
+height, width, layers = frame.shape
+# setting the frame rate (here it is 7) and video name and sizes
 video = cv2.VideoWriter(video_name, 0, 7, (width,height))
 
-for image in images:
-    video.write(cv2.imread(os.path.join(image_folder, image)))
-
+for image in np_img:
+    video.write(cv2.imread(os.path.join(image_folder, str(image)+'.jpg')))
 cv2.destroyAllWindows()
 video.release()
