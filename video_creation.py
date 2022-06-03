6da54dad12f168a5944fdb9257e3838b1bf433b9
SabaJazi <saba.yousefian72@gmail.com>
2022-06-02 20:21:37 -0500

    eddit code of video

--- video_creation.py
+++ video_creation.py
@@ -1,14 +1,16 @@
+import numbers
+from tokenize import Number
 import cv2
 import os
 
-image_folder = 'images'
-video_name = 'video.avi'
-
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+image_folder = 'd:/LTT/frames'
+video_name = 'original.avi'
+#what is the type of os.listdr??
+images = [img for img in sorted(os.listdir(image_folder)) if img.endswith(".jpg")]
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 
-video = cv2.VideoWriter(video_name, 0, 1, (width,height))
+video = cv2.VideoWriter(video_name, 0, 7, (width,height))
 
 for image in images:
     video.write(cv2.imread(os.path.join(image_folder, image)))
